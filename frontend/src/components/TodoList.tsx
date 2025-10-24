f473f134ab5218b156c2c1f21afb8c5334435030
vishalm342 <vishalm8656@gmail.com>
2025-10-24 09:41:27 +0530

    Added empty message for TodoList component

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -2,6 +2,7 @@ import useTodoLists from "hooks/useTodoLists";
 import { FC } from "react";
 import Loading from "components/Loading";
 import TodoCard from "./TodoCard";
+import { BsClipboardCheck } from "react-icons/bs";
 
 const TodoList: FC = () => {
     const { todo, loading } = useTodoLists();
@@ -10,11 +11,23 @@ const TodoList: FC = () => {
             {
                 loading
                     ? <Loading />
-                    : (
-                        <div className="flex gap-5 flex-col">
-                            {todo?.map((el) => <TodoCard key={el.id} id={el.id} status={el.status} task={el.task} />)}
-                        </div>
-                    )
+                    : todo && todo.length > 0 
+                        ? (
+                            <div className="flex gap-5 flex-col">
+                                {todo.map((el) => <TodoCard key={el.id} id={el.id} status={el.status} task={el.task} />)}
+                            </div>
+                        )
+                        : (
+                            <div className="flex flex-col items-center justify-center h-full text-center">
+                                <BsClipboardCheck className="text-6xl text-gray-400 mb-4" />
+                                <h2 className="text-2xl font-semibold text-gray-600 mb-2">
+                                    No Tasks added yet!
+                                </h2>
+                                <p className="text-lg text-blue-500 font-medium">
+                                    Add your first task of the day!
+                                </p>
+                            </div>
+                        )
             }
         </div>
     )
